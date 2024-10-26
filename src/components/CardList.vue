3057cccbd1fb19fa9a51075ad28321f36789cbbe
MAXSSON <kaprielovmaksim@mail.ru>
2024-10-26 20:12:20 +0300

    Настроено графическое окружение, созданы правила для ESLint, сверстан Header, карточка товара (Сard.vue) и сетка для расположения списка (CardList.vue)

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from './Card.vue';
+
+const onClickAdd = () => {
+    alert('Добавить!');
+}
+</script>
+
+<template>
+    <div class="grid grid-cols-4 gap-5">
+        <Card 
+        title="Мужские кроссовки Nike Blazer Mid Suede" 
+        imageUrl="/sneakers/sneakers-1.jpg" 
+        :price="1205" 
+        :isAdded="true"
+        :isFavourite="false"
+        :onClickAdd="onClickAdd"
+        />
+    </div>
+</template>
